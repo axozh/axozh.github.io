f5d2347ae913b68fd789a0087ad3a9a78500f6fd
axozh <141644849+elite2site@users.noreply.github.com>
2023-10-25 18:13:11 -0400

    ...

--- search.js
+++
@@ -1,8 +0,0 @@
-function animateSearchBar(isActive) {
-  const searchContainer = document.querySelector('.search');
-  if (isActive) {
-    searchContainer.classList.add('active');
-  } else {
-    searchContainer.classList.remove('active');
-  }
-}
